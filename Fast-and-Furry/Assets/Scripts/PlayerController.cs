b2c230e997ffeaaf19cd830bdc04b6f0545c81fd
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-01-17 11:14:55 +0100

    Basic gyroscope

---
+++ Fast-and-Furry/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Gyroscope gyroscope;
+
+    private void Awake()
+    {
+        //gyroscope.enabled = true;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.Log(SystemInfo.supportsGyroscope);
+    }
+}
