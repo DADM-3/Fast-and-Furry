f8c8d84479ffbed6d59097831d46ac8cd98adf1f
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-01-17 14:14:14 +0100

    Basic mechanics and extra packages imported

--- Fast-and-Furry/Assets/Scripts/PlayerController.cs
+++ Fast-and-Furry/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+    //Components references
+    private Rigidbody rb;
+
+    //Gyroscope management
     private Gyroscope gyroscope;
 
+    //Movement
+    [SerializeField]
+    private float frontalMoveSpeed;
+    [SerializeField]
+    private float lateralMoveSpeed;
+    private Vector3 velocity;
+
+    //Jump
+    [SerializeField]
+    private float jumpForce;
+    private Vector3 startSwipe, endSwipe;
+    private bool jumping;
+
+    //Camera follow
+    [SerializeField]
+    private Transform shouldersFollow;
+    #endregion
+
+    #region Unity Callbacks
     private void Awake()
     {
-        //gyroscope.enabled = true;
-    }
-
-    void Start()
-    {
-
+        rb = GetComponent<Rigidbody>();
+        EnableGyroscope();
     }
 
     void Update()
     {
-        Debug.Log(SystemInfo.supportsGyroscope);
+        shouldersFollow.transform.position = new Vector3(transform.position.x, shouldersFollow.transform.position.y, transform.position.z);
+        velocity = new Vector3(gyroscope.attitude.normalized.x * lateralMoveSpeed, rb.velocity.y, frontalMoveSpeed);
+        JumpSwipeCheck();
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = velocity;
+        JumpIfAllowed();
+    }
+    #endregion
+
+    #region My functions
+    private void EnableGyroscope()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+        }
+    }
+
+    private void JumpSwipeCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startSwipe = Input.GetTouch(0).position;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endSwipe = Input.GetTouch(0).position;
+            if (endSwipe.y > startSwipe.y && rb.velocity.y == 0)
+                jumping = true;
+        }
+    }
+
+    private void JumpIfAllowed()
+    {
+        if (jumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumping = false;
+        }
+    }
+    #endregion
 }
