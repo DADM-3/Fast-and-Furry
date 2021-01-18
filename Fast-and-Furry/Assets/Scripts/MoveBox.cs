23cd84f047b885a42c783f55e977d4acd8828ec3
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-01-18 23:48:55 +0100

    Moving Boxes and map swidth fixed

---
+++ Fast-and-Furry/Assets/Scripts/MoveBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBox : MonoBehaviour
+{
+    #region Variables
+    //Movement
+    public float speed;
+
+    //Auxiliar
+    private bool moveRight;
+    private float targetRight;
+    private float targetLeft;
+    public float leeway;
+    #endregion
+
+    #region Unity callbacks
+    private void Start()
+    {
+        targetRight = transform.localPosition.x + leeway;
+        targetLeft = transform.localPosition.x - leeway;
+    }
+    private void Update()
+    {
+        if (moveRight)
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        else
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+        if (transform.localPosition.x >= targetRight)
+            moveRight = false;
+
+        if (transform.localPosition.x <= targetLeft)
+            moveRight = true;
+    }
+    #endregion
+}
--- Fast-and-Furry/Assets/Scripts/ScreensManager.cs
+++ Fast-and-Furry/Assets/Scripts/ScreensManager.cs
@@ -25,5 +25,10 @@ public class ScreensManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void GoToURL(string url)
+    {
+        Application.OpenURL(url);
+    }
     #endregion
 }
